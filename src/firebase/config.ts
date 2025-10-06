3f77aa61cfc448c40f1c64039af94294ef75aaae
Mauro Bordon <maurofbordon@gmail.com>
2025-10-06 23:34:07 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1142623020-ef685",
+  "appId": "1:561293876569:web:497eb55d88ee5ff303c048",
+  "apiKey": "AIzaSyDSBbYd5zxeUzQGyUOqgsGnIYAvRSkAnWs",
+  "authDomain": "studio-1142623020-ef685.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "561293876569"
+};
