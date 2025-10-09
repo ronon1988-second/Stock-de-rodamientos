f55cd2594803fb15152aa25f4e2d6c21db9f2ed4
Mauro Bordon <maurofbordon@gmail.com>
2025-10-09 17:17:56 +0000

    dale te doy permiso para revisar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -856,7 +856,7 @@ function AppContent() {
           )}
         </header>
         <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 bg-muted/40">
-          <div className="overflow-x-auto">
+          <div>
             {renderContent()}
           </div>
         </main>
@@ -887,3 +887,5 @@ export default function Page() {
 
   return <AppContent />;
 }
+
+    
