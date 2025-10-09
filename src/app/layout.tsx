a9061b8de3f8769d480d8944cd5b350e94b81ff4
Mauro Bordon <maurofbordon@gmail.com>
2025-10-09 14:14:43 +0000

    hay algo q no esta bien, en movil sigo teniendo que hacer scroll lateral

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="es" suppressHydrationWarning>
        <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
